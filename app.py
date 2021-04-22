d248a8f42ccdd9d325113a93e98cf12f8f9d21ec
J.Shin <bonanob@hotmail.com>
2021-04-22 14:56:46 +0200

    functions added: add_product(), export_csv(), view_product(), app()

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import csv
 import datetime
+import time
+
 
 from models import Base, session, engine, Product
 
@@ -8,24 +10,25 @@ def menu():
     while True:
         choice = input('''
             \nSTORE INVENTORY
-            \rv > View Singel Product
+            \rv > View Single Product
             \ra > Add New Product
             \rb > Backup Entire Inventory
+            \rq > Quit
             \rWhat would you like to do? ''')
-        if choice.lower() in ('v', 'a', 'b'):
-            return choice
+        if choice.lower() in ('v', 'a', 'b', 'q'):
+            return choice.lower()
         else:
             input('''
-                  \nPlease choose one of the options above(v, a or b).
+                  \nPlease choose one of the options above(v, a, b or q).
                   \rPress enter to try again.''')
 
 
 def clean_price(price_str):
     try:
-        # try:
-        #     price_float = float(price_str)
-        # except ValueError:
-        price_float = float(price_str[1:])
+        try:
+            price_float = float(price_str)
+        except ValueError:
+            price_float = float(price_str[1:])
 
     except ValueError:
         input('''
@@ -54,6 +57,102 @@ def clean_date(date_str):
         return return_date
 
 
+def clean_id(id_str):
+    id_options = []
+    for product in session.query(Product):
+        id_options.append(product.product_id)
+    try:
+        product_id = int(id_str)
+    except ValueError:
+        input('''
+              \n***** ID ERROR *****
+              \rID should be a number.
+              \rPress enter to try again.
+              \r********************''')
+        return
+    else:
+        if product_id in id_options:
+            return product_id
+        else:
+            input(f'''
+                  \n***** ID ERROR *****
+                  \rOptions: {id_options}
+                  \rPress enter to try again.
+                  \r********************''')
+            return
+
+
+def view_product():
+    id_error = True
+    while id_error == True:
+        product_id = input('What is the the product ID? ')
+        product_id = clean_id(product_id)
+        if type(product_id) == int:
+            id_error = False
+    product_viewed = session.query(Product).filter(
+        Product.product_id == product_id).one_or_none()
+    print(f'''
+            \n--------------------------------------------
+            \rName: {product_viewed.product_name}
+            \rPRICE: {product_viewed.product_price}
+            \rQTY:{product_viewed.product_quantity}
+            \rDate Updated: {product_viewed.date_updated}
+            \r--------------------------------------------''')
+    time.sleep(2)
+
+
+def add_product():
+    name = input('What is the name of the product? ')
+
+    quantity_error = True
+    while quantity_error == True:
+        quantity = input('How many are there? ')
+        try:
+            int(quantity)
+        except ValueError:
+            quit_or_again = input('''
+                    \nThe quantity must be an number.
+                    \rPress enter to try again.''')
+        else:
+            quantity_error = False
+
+    price_error = True
+    while price_error == True:
+        price = input('What is the price of the product?(ex. 3.99) ')
+        price = clean_price(price)
+        if type(price) == int:
+            price_error = False
+
+    date = datetime.datetime.now()
+
+    # Delete conflicting instance.
+    session.query(Product).filter(name == Product.product_name).delete()
+
+    new_product = Product(product_name=name, product_quantity=quantity,
+                          product_price=price, date_updated=date)
+    session.add(new_product)
+    session.commit()
+    print(f'-- {name} was added. --')
+    time.sleep(1)
+
+
+def export_csv():
+    with open('backup.csv', 'w', newline='') as csvfile:
+        fieldnames = Product.__table__.columns.keys()
+        product_writer = csv.DictWriter(
+            csvfile, fieldnames=fieldnames, lineterminator='\n')
+
+        product_writer.writeheader()
+        for product in session.query(Product):
+            product_writer.writerow({'product_id': product.product_id,
+                                     'product_name': product.product_name,
+                                     'product_quantity': product.product_quantity,
+                                     'product_price': product.product_price,
+                                     'date_updated': product.date_updated})
+    print('-- Export successful. --')
+    time.sleep(1)
+
+
 def add_csv():
     with open('inventory.csv') as csvfile:
         data = csv.reader(csvfile)
@@ -61,22 +160,44 @@ def add_csv():
         for row in data:
             product_in_db = session.query(Product).filter(
                 Product.product_name == row[0]).one_or_none()
+            name = row[0]
+            price = clean_price(row[1])
+            quantity = int(row[2])
+            date = clean_date(row[3])
+            new_product = Product(
+                product_name=name, product_quantity=quantity, product_price=price, date_updated=date)
+
             if product_in_db == None:
-                name = row[0]
-                price = clean_price(row[1])
-                quantity = int(row[2])
-                date = clean_date(row[3])
-                new_product = Product(
-                    product_name=name, product_quantity=quantity, product_price=price, date_updated=date)
                 session.add(new_product)
+            else:
+                # TODO: change variables. make it more readable
+                db_time = product_in_db.date_updated
+                db_time = datetime.datetime(
+                    db_time.year, db_time.month, db_time.day)
+                if clean_date(row[3]) > db_time:
+                    session.query(Product).filter(
+                        Product.product_name == row[0]).delete()
+                    session.add(new_product)
+            session.flush()
         session.commit()
 
 
 def app():
-    menu()
+    app_running = True
+    while app_running:
+        menu_choice = menu()
+        if menu_choice == 'v':
+            view_product()
+        elif menu_choice == 'a':
+            add_product()
+        elif menu_choice == 'b':
+            export_csv()
+        else:
+            print('-- GOODBYE. --')
+            app_running = False
 
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
-    # menu()
     add_csv()
+    app()
